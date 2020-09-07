2d9cfc402947ca5b0ac66951405db5a52f37a3a1
ren_higuchi <ren_higuchi@change-jp.com>
2020-09-07 21:04:38 +0900

    init

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          testes,hohoge
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
